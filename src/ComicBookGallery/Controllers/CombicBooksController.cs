a4c4fb4c775d1cf6d8838124fff52c2eed01fa57
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2017-08-20 22:40:37 -0500

    Added comic book repository

--- src/ComicBookGallery/Controllers/CombicBooksController.cs
+++ src/ComicBookGallery/Controllers/CombicBooksController.cs
@@ -15,15 +15,16 @@ namespace ComicBookGallery.Controllers
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                 {
-                    new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"}
-                }
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
+                },
+                Favorite = false
             };
 
             return View(comicBook);
